e75060784358ae95c544f2b6b5472db40ea40b14
Federico Benitez <federico@nubloq.co>
2023-09-25 11:08:24 -0500

    Ajuste de autofocus en mobile

--- src/script.js
+++ src/script.js
@@ -2,12 +2,13 @@ var conversationalForm = window.cf.ConversationalForm.startTheConversation({
   formEl: document.getElementById("form"),
   context: document.getElementById("cf-context"),
   userImage: "text:ME",
-  robotImage:
-    "src/Favicon-8.png",
-  submitCallback: function() {
+  robotImage: "src/Favicon-8.png",
+  submitCallback: function () {
     alert(
       "Custom submit callback reached, removing Conversational Form, see markup of this file"
     );
   },
-  showProgressBar: true
+  showProgressBar: true,
+  preventAutoFocus: true,
+
 });
