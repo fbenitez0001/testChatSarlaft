325fa25036901533ff0f537c5bbfb01bac625c6f
Federico Benitez <federico@nubloq.co>
2023-09-25 10:35:04 -0500

    <Commit desde terminal>

---
+++ src/script.js
@@ -0,0 +1,13 @@
+var conversationalForm = window.cf.ConversationalForm.startTheConversation({
+  formEl: document.getElementById("form"),
+  context: document.getElementById("cf-context"),
+  userImage: "text:ME",
+  robotImage:
+    "src/Favicon-8.png",
+  submitCallback: function() {
+    alert(
+      "Custom submit callback reached, removing Conversational Form, see markup of this file"
+    );
+  },
+  showProgressBar: true
+});
